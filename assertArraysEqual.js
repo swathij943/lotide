f46e6aa3c4e4f03c655c6fbfe516242daba864ed
swathij943 <swathij943@gmail.com>
2023-04-22 14:23:28 -0400

    Added assigment on eqArrays.js - Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arrays1, arrays2) {
+  for(i = 0; i < arrays1.length; i++) {
+    if(arrays1[i] === arrays2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+}
+
+const assetArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(' ✅✅✅ Assertion Passed: ', actual, ' === ', expected);
+} else {
+  console.log(' 🛑🛑🛑 Assertion Failed: ', actual, ' !== ', expected);
+}
+};
