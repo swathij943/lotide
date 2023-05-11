1ba6864a7d7f423f00f3edd443b16333887e1680
swathij943 <swathij943@gmail.com>
2023-05-11 07:15:23 -0400

    head.js & headTest.js - Incoporate Mocha & Chai Lotide Assignment

---
+++ test/README.md
@@ -0,0 +1,9 @@
+#Lotide
+
+The Lotide project is inspired by the Lodash project, which is a utility library for JavaScript. While Lodash itself is quite powerful, our version of it is simpler in order to focus on learning the fundamentals of programming and data management using JavaScript.
+
+The Lotide project is a modular collection of functions that operate on data, namely Arrays and Objects. In creating these functions, we will become more comfortable with FOCAL as well as writing modular code and Automated Testing (by writing unit tests). At first, we create our own assertion functions for writing test code, but by the end of the project we use industry-standard libraries Mocha and Chai for our automated testing. Through Lotide, we also learn about software dependencies, package management, and npm, the package manager of choice in the Node community. We learn that anyone can publish their own npm package by publishing Lotide.
+
+## Purpose
+
+This project was published and created for my learning.
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,27 @@
-
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([42]), 42);
+describe('#head', () => {
+  it('should return the first element of an array', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('should return the first element of an array of strings', () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it('shoild return undefined if the array is empty', () => {
+    assert.isUndefined(head([]));
+  });
+  it('should return the only element of an array with a single element', () => {
+    assert.strictEqual(head([42]), 42);
+  });
+});
+
+
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
+
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+// assertEqual(head([42]), 42);
