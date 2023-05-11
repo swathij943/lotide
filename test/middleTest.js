d3ab8ee542ff8ec02a1b329b194aeaf91b6e86e7
swathij943 <swathij943@gmail.com>
2023-05-11 07:43:11 -0400

    Refactor tailTest.js & middleTest.js - Incoporate Mocha & Chai Lotide Assignment

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,26 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3]), [3]);
-assertArraysEqual(middle('mid', [1])); // => []
-assertArraysEqual(middle([1, 2])); // => []
-assertArraysEqual(middle([1, 2, 3])); // => [2]
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  
+  it("returns [] for ['mid', [1]]", () => {
+    assert.deepEqual(middle(['mid', [1]]), []);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
+
+
+// assertArraysEqual(middle([1, 2, 3]), [3]);
+// assertArraysEqual(middle('mid', [1])); // => []
+// assertArraysEqual(middle([1, 2])); // => []
+// assertArraysEqual(middle([1, 2, 3])); // => [2]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,22 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-assertEqual(tail([5, 6, 7]), [6, 7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(tail([]), []);
-assertEqual(tail([42]), []);
+describe("#tail", () => {
+  it("returns [6,7] for [5,6,7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("returns [] for [42]", () => {
+    assert.deepEqual(tail([42]), []);
+  });
+});
+
+// assertEqual(tail([5, 6, 7]), [6, 7]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+// assertEqual(tail([]), []);
+// assertEqual(tail([42]), []);
